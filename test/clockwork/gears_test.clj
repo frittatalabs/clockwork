0d199da43ed9067938c71e26cea892a3eb1278cd
Paolo Ferri <paolo@frittatalabs.com>
2025-08-25 20:48:53 -0700

    property based tests

--- test/clockwork/gears_test.clj
+++ test/clockwork/gears_test.clj
@@ -1,5 +1,8 @@
 (ns clockwork.gears-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is]]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
             [clockwork.gears :as g]))
 
 ;; --- scaffolding ---
@@ -81,6 +84,70 @@
       (is (vector? q))
       (is (empty? q)))))
 
-(deftest engage-embeds-value
-  (doseq [v [0 1 -3 42]]
-    (is (= [:embedded v] ((g/engage v) driver)))))
+;; --- Property-based tests ---
+
+(def pure-fns
+  [inc
+   #(* 2 %)
+   #(+ 10 %)
+   dec
+   identity])
+
+(def gear-fns
+  [step-inc
+   step-times2
+   step-add10
+   (fn [x] (g/simple (- x)))])
+
+(def gen-pure-fn (gen/elements pure-fns))
+(def gen-gear-fn (gen/elements gear-fns))
+
+(def gen-pure-fn-seq (gen/vector gen-pure-fn 0 5))
+(def gen-gear-fn-seq (gen/vector gen-gear-fn 0 5))
+
+;; --- Properties ---
+
+(defspec xform-vs-reduce-xform*
+  50
+  (prop/for-all [init gen/int
+                 fs gen-pure-fn-seq]
+    (let [cw1 (apply g/xform (g/simple init) fs)
+          cw2 (reduce (fn [gear f] (g/xform* gear f))
+                      (g/simple init)
+                      fs)]
+      (= (cw1 driver) (cw2 driver)))))
+
+(defspec mesh-vs-reduce-mesh*
+  50
+  (prop/for-all [init gen/int
+                 fs gen-gear-fn-seq]
+    (let [cw1 (apply g/mesh (g/simple init) fs)
+          cw2 (reduce (fn [gear f] (g/mesh* gear f))
+                      (g/simple init)
+                      fs)]
+      (= (cw1 driver) (cw2 driver)))))
+
+(defspec complect-empty-equals-engage
+  50
+  (prop/for-all [v gen/int]
+    (= ((g/complect v []) driver)
+       ((g/engage v) driver))))
+
+(defspec xform-then-mesh-equivalence
+  50
+  (prop/for-all [init gen/int
+                 pfs  gen-pure-fn-seq
+                 gfs  gen-gear-fn-seq]
+    (let [cw1 (as-> (g/engage init) clock
+                (apply g/xform clock pfs)
+                (apply g/mesh  clock gfs))
+          cw2 (as-> (g/engage init) clock
+                  (reduce g/xform* clock pfs)
+                  (reduce g/mesh* clock gfs))]
+      (or (empty? gfs)
+          (= (cw1 driver) (cw2 driver))))))
+
+(defspec engage-embeds-value
+  50
+  (prop/for-all [v gen/int]
+    (= [:embedded v] ((g/engage v) driver))))
